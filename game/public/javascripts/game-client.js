763e56cf524141dbaa504421a9f8704309e58948
lim-lim-lim <limlimlim.dev@gmail.com>
2016-01-22 18:45:19 +0900

    .

--- game/public/javascripts/game-client.js
+++ game/public/javascripts/game-client.js
@@ -2,8 +2,20 @@
 
 
 +function(){
-    angular.module( 'Game', [] )
-        .controller( 'GameController', function(){
+    var TITLE = 'Team Dodge'
+    angular.module( 'Game', [ 'ngRoute' ] )
+        .config( [ '$routeProvider', function( $routeProvider ){
 
+            $routeProvider.when( '/watingRoom', {
+                templateUrl : 'wating-room.html',
+                controller : ''
+            } )
+        }])
+        .controller( 'GameController', function( $scope ){
+            $scope.title = TITLE;
+
+            $scope.goWatingRoom = function(){
+
+            }
         });
 }();
